fb0472d5e79946643b1cd5f3c51921aabe226251
habdig7oficial <65318468+habdig7oficial@users.noreply.github.com>
2021-10-20 18:25:16 -0300

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,30 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
 
+function criar (cl1){
+
+  let div1= document.createElement("div");
+
+  let div2= document.createElement("div");
+
+  let div3= document.createElement("div");
+
+
+  let titulo = document.createElement("h3");
+  let imagem = document.createElement("img");
+
+  let titulo = document.createElement("h3");
+
+
+  div1.className = cl1;
+
+}
+
+
+
+
+
   });
+
+
+
